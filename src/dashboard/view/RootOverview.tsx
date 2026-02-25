d1882ddc53816c58122e046443cb8de777eabdef
Stijn Van Doorslaer <stijn@codeurs.be>
2026-02-25 11:36:01 +0100

    Splash page

--- src/dashboard/view/RootOverview.tsx
+++ src/dashboard/view/RootOverview.tsx
@@ -1,9 +1,12 @@
 import styler from '@alinea/styler'
 import type {RootData} from 'alinea/core/Root'
+import {object} from 'alinea/field.js'
 import {HStack, Icon, TextLabel, Typo, VStack} from 'alinea/ui'
 import {IcRoundDescription} from 'alinea/ui/icons/IcRoundDescription'
+import {IcRoundInsertDriveFile} from 'alinea/ui/icons/IcRoundInsertDriveFile'
 import {Lift} from 'alinea/ui/Lift'
 import {Main} from 'alinea/ui/Main'
+import Image from 'next/image'
 import {useWorkspace} from '../hook/UseWorkspace.js'
 import {Head} from '../util/Head.js'
 import css from './RootOverview.module.scss'
@@ -20,61 +23,64 @@ const examplePages = [
     title: 'Home',
     url: '/en',
     type: 'HomeSchema',
-    lastModified: '10/01/2026'
+    lastModified: '05/01/2026'
   },
   {
     title: 'About us',
     url: '/en/about',
     type: 'PageSchema',
-    lastModified: '09/01/2026'
+    lastModified: '04/01/2026'
   },
   {
     title: 'Services',
     url: '/en/services',
     type: 'PageSchema',
-    lastModified: '08/01/2026'
-  },
-  {
-    title: 'Our Team',
-    url: '/en/team',
-    type: 'PageSchema',
-    lastModified: '07/01/2026'
-  },
-  {
-    title: 'Portfolio',
-    url: '/en/portfolio',
-    type: 'PageSchema',
-    lastModified: '06/01/2026'
+    lastModified: '03/01/2026'
   },
   {
     title: 'News',
     url: '/en/news',
     type: 'NewsSchema',
-    lastModified: '05/01/2026'
-  },
-  {
-    title: 'News detail',
-    url: '/en/news/detail',
-    type: 'NewsDetailSchema',
-    lastModified: '04/01/2026'
+    lastModified: '02/01/2026'
   },
   {
     title: 'FAQ',
     url: '/en/faq',
     type: 'PageSchema',
-    lastModified: '03/01/2026'
-  },
-  {
-    title: 'Contact',
-    url: '/en/contact',
-    type: 'PageSchema',
-    lastModified: '02/01/2026'
-  },
-  {
-    title: 'Privacy Policy',
-    url: '/en/privacy-policy',
-    type: 'PageSchema',
     lastModified: '01/01/2026'
+  }
+]
+
+const exampleImages = [
+  {
+    url: 'https://picsum.photos/64',
+    title: 'artem-stoliar-CF-2tl6MQj0-unsplash',
+    extension: 'jpg'
+  },
+  {
+    url: 'https://picsum.photos/64',
+    title: 'nir-himi-KSlJhcpDaQk-unsplash',
+    extension: 'png'
+  },
+  {
+    url: 'https://picsum.photos/64',
+    title: 'ingmar-h-SvFFr464urs-unsplash',
+    extension: 'jpg'
+  }
+]
+
+const exampleFiles = [
+  {
+    title: 'lorem-ipsum-dolor-sit-amet',
+    extension: 'pdf'
+  },
+  {
+    title: 'consectetur-adipiscing-elit',
+    extension: 'pdf'
+  },
+  {
+    title: 'nullam mi est, mollis vel tortor non',
+    extension: 'docx'
   }
 ]
 
@@ -154,6 +160,51 @@ export function RootOverview({root}: RootOverviewProps) {
                     ))}
                   </tbody>
                 </table>
+              </VStack>
+            </HStack>
+          </Lift>
+          <Lift className={styles.root.lift()}>
+            <HStack gap={64} wrap>
+              <VStack gap={8} style={{flex: '1 1 320px'}}>
+                <Typo.H2
+                  style={{
+                    paddingBottom: '8px',
+                    borderBottom: '1px solid var(--alinea-outline)'
+                  }}
+                >
+                  <TextLabel label="Last uploaded images" />
+                </Typo.H2>
+                {exampleImages.map((image, index) => (
+                  <div className={styles.listItem()} key={index}>
+                    <div className={styles.listItem.image()}>
+                      <img
+                        src={image.url}
+                        width={64}
+                        height={64}
+                        style={{objectFit: 'cover'}}
+                      />
+                    </div>
+                    {image.title}.{image.extension}
+                  </div>
+                ))}
+              </VStack>
+              <VStack gap={8} style={{flex: '1 1 320px'}}>
+                <Typo.H2
+                  style={{
+                    paddingBottom: '8px',
+                    borderBottom: '1px solid var(--alinea-outline)'
+                  }}
+                >
+                  <TextLabel label="Last uploaded files" />
+                </Typo.H2>
+                {exampleFiles.map((file, index) => (
+                  <div className={styles.listItem()} key={index}>
+                    <div className={styles.listItem.file(file.extension)}>
+                      <Icon icon={IcRoundInsertDriveFile} size={32} />
+                    </div>
+                    {file.title}.{file.extension}
+                  </div>
+                ))}
               </VStack>
             </HStack>
           </Lift>
